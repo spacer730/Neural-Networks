538f95bd23d87cd7ccf1e1c986ad61fc599edb18
Hossein Hashemi <hashemi@ijssel.strw.leidenuniv.nl>
2018-02-27 14:55:12 +0100

    Finished Assignment 1-2

--- Assignment 1/Assignment-1_2.py
+++ Assignment 1/Assignment-1_2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.ma as ma
 from sklearn.metrics import confusion_matrix
+import sklearn
 import matplotlib.pyplot as plt
 import csv
 
@@ -13,7 +14,7 @@ with open('train_out.csv') as train_out:
 	readtrain_out = csv.reader(train_out, delimiter=',')
 	for row in readtrain_out:
 		for i in range(10):
-			if int(row[0])==i:			
+			if int(row[0])==i:
 				digitsoflines[i].append(readtrain_out.line_num) #Read out which lines belong to which digits
 
 n=[len(digitsoflines[i]) for i in range(10)] #Compute number of training points for each digit
@@ -29,57 +30,122 @@ with open('train_in.csv') as train_in:
 for i in range(10):
 	c[i]=Sum[i]/n[i] #Compute the center of each digit by dividing the sum of all the training points by the number of training points for each digit
 
-trainprediction=[]
+trainpredictioneuclid=[]
+trainpredictionmanhattan=[]
+trainpredictioncosine=[]
+trainpredictioncorrelation=[]
 		
-with open('train_in.csv') as train_in:
+with open('train_in.csv') as train_in: #Compute distance between centers and data point and prediction is closest center
 	readtrain_in = csv.reader(train_in, delimiter=',')
 	for row in readtrain_in:
-		dist=[]
+		disteuclid=[]
+		distmanhattan=[]
+		distcosine=[]
+		distcorrelation=[]
 		for i in range(10): #Find out which digit the row is
-			dist.append(np.dot(c[i]-np.array(list(map(float,row))),c[i]-np.array(list(map(float,row)))))
-		trainprediction.append(np.argmin(dist))
-
-print(len(trainprediction))
+			disteuclid.append(np.dot(c[i]-np.array(list(map(float,row))),c[i]-np.array(list(map(float,row)))))
+			distmanhattan.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='manhattan'))
+			distcosine.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='cosine'))
+			distcorrelation.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='correlation'))
+			
+		trainpredictioneuclid.append(np.argmin(disteuclid))
+		trainpredictionmanhattan.append(np.argmin(distmanhattan))
+		trainpredictioncosine.append(np.argmin(distcosine))
+		trainpredictioncorrelation.append(np.argmin(distcorrelation))
 
 traintrue=[]
-counter=0
+traincountereuclid=0
+traincountermanhattan=0
+traincountercosine=0
+traincountercorrelation=0
 
 with open('train_out.csv') as train_out:
 	readtrain_out = csv.reader(train_out, delimiter=',')
 	for row in readtrain_out:
 		traintrue.append(int(row[0]))
-		if int(row[0])==trainprediction[readtrain_out.line_num-1]:
-			counter+=1
+		if int(row[0])==trainpredictioneuclid[readtrain_out.line_num-1]:
+			traincountereuclid+=1
+		if int(row[0])==trainpredictionmanhattan[readtrain_out.line_num-1]:
+			traincountermanhattan+=1
+		if int(row[0])==trainpredictioncosine[readtrain_out.line_num-1]:
+			traincountercosine+=1
+		if int(row[0])==trainpredictioncorrelation[readtrain_out.line_num-1]:
+			traincountercorrelation+=1
 
-print(confusion_matrix(trainprediction,traintrue))
+print("Confusion matrix for training set using euclidean distance:")
+print(confusion_matrix(trainpredictioneuclid,traintrue))
+print("Confusion matrix for training set using manhattan distance:")
+print(confusion_matrix(trainpredictionmanhattan,traintrue))
+print("Confusion matrix for training set using cosine distance:")
+print(confusion_matrix(trainpredictioncosine,traintrue))
+print("Confusion matrix for training set using correlation distance:")
+print(confusion_matrix(trainpredictioncorrelation,traintrue))
 
-print(counter)
-print(100*counter/len(trainprediction))
+print("The accuracy of the euclidean distance algorithm on the training set is:")
+print(100*traincountereuclid/len(trainpredictioneuclid))
+print("The accuracy of the manhattan distance algorithm on the training set is:")
+print(100*traincountermanhattan/len(trainpredictionmanhattan))
+print("The accuracy of the cosine distance algorithm on the training set is:")
+print(100*traincountercosine/len(trainpredictioncosine))
+print("The accuracy of the correlation distance algorithm on the training set is:")
+print(100*traincountercorrelation/len(trainpredictioncorrelation))
 
-testprediction=[]
-		
-with open('test_in.csv') as test_in:
+testpredictioneuclid=[]
+testpredictionmanhattan=[]
+testpredictioncosine=[]
+testpredictioncorrelation=[]
+
+with open('test_in.csv') as test_in: #Compute distance between centers and data point and prediction is closest center
 	readtest_in = csv.reader(test_in, delimiter=',')
 	for row in readtest_in:
-		dist=[]
+		disteuclid=[]
+		distmanhattan=[]
+		distcosine=[]
+		distcorrelation=[]
 		for i in range(10): #Find out which digit the row is
-			dist.append(np.dot(c[i]-np.array(list(map(float,row))),c[i]-np.array(list(map(float,row)))))
-		testprediction.append(np.argmin(dist))
-
-print(len(testprediction))
+			disteuclid.append(np.dot(c[i]-np.array(list(map(float,row))),c[i]-np.array(list(map(float,row)))))
+			distmanhattan.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='manhattan'))
+			distcosine.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='cosine'))
+			distcorrelation.append(sklearn.metrics.pairwise.pairwise_distances([c[i]],[np.array(list(map(float,row)))],metric='correlation'))
+			
+		testpredictioneuclid.append(np.argmin(disteuclid))
+		testpredictionmanhattan.append(np.argmin(distmanhattan))
+		testpredictioncosine.append(np.argmin(distcosine))
+		testpredictioncorrelation.append(np.argmin(distcorrelation))
 
 testtrue=[]
-counter=0
+testcountereuclid=0
+testcountermanhattan=0
+testcountercosine=0
+testcountercorrelation=0
 
-counter2=0
 with open('test_out.csv') as test_out:
 	readtest_out = csv.reader(test_out, delimiter=',')
 	for row in readtest_out:
 		testtrue.append(int(row[0]))
-		if int(row[0])==testprediction[readtest_out.line_num-1]:
-			counter2+=1
+		if int(row[0])==testpredictioneuclid[readtest_out.line_num-1]:
+			testcountereuclid+=1
+		if int(row[0])==testpredictionmanhattan[readtest_out.line_num-1]:
+			testcountermanhattan+=1
+		if int(row[0])==testpredictioncosine[readtest_out.line_num-1]:
+			testcountercosine+=1
+		if int(row[0])==testpredictioncorrelation[readtest_out.line_num-1]:
+			testcountercorrelation+=1
 
-print(confusion_matrix(testprediction,testtrue))
+print("Confusion matrix for test set using euclidean distance:")
+print(confusion_matrix(testpredictioneuclid,testtrue))
+print("Confusion matrix for test set using manhattan distance:")
+print(confusion_matrix(testpredictionmanhattan,testtrue))
+print("Confusion matrix for test set using cosine distance:")
+print(confusion_matrix(testpredictioncosine,testtrue))
+print("Confusion matrix for test set using correlation distance:")
+print(confusion_matrix(testpredictioncorrelation,testtrue))
 
-print(counter2)
-print(100*counter2/len(testprediction))
+print("The accuracy of the euclidean distance algorithm on the test set is:")
+print(100*testcountereuclid/len(testpredictioneuclid))
+print("The accuracy of the manhattan distance algorithm on the test set is:")
+print(100*testcountermanhattan/len(testpredictionmanhattan))
+print("The accuracy of the cosine distance algorithm on the test set is:")
+print(100*testcountercosine/len(testpredictioncosine))
+print("The accuracy of the correlation distance algorithm on the test set is:")
+print(100*testcountercorrelation/len(testpredictioncorrelation))
